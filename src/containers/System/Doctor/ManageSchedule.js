57e4df1453af111ac24b26252df5aa0a2dbf1e18
nguyenanhvu03 <nguyenanhvu.tdc2223@gmail.com>
2024-05-31 23:04:11 +0700

    commit message

--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -1,24 +1,116 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import './ManageSchedule.scss'
+import { FormattedMessage } from 'react-intl';
+import Select from 'react-select';
+import * as actions from '../../../store/actions';
+import { CRUD_ACTIONS, LANGUAGES } from '../../../utils';
+import DatePicker from '../../../components/Input/DatePicker';
+import moment from 'moment';
+
+
+
+
 
 class ManageSchedule extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            listDoctors: [],
+            selectedDoctor: {},
+            currentDate: ''
+        }
+    }
+    componentDidMount() {
+        this.props.fetchAllDoctors();
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.allDoctors !== this.props.allDoctors) {
+            let dataSelect = this.buidDataInputSelect(this.props.allDoctors)
+            this.setState({
+                listDoctors: dataSelect
+            })
+        }
+        // if (prevProps.language !== this.props.language) {
+        //     let dataSelect = this.buidDataInputSelect(this.props.allDoctors)
+        //     this.setState({
+        //         listDoctors: dataSelect
+        //     })
+        // }
+    }
+    buidDataInputSelect = (inputData) => {
+        let result = [];
+        let { language } = this.props;
+        if (inputData && inputData.length > 0) {
+            inputData.map((item, index) => {
+                let Object = {};
+                let labelVi = `${item.lastName} ${item.firstName}`
+                let labelEn = `${item.firstName} ${item.lastName}`
+                Object.label = language === LANGUAGES.VI ? labelVi : labelEn;
+                Object.value = item.id;
+                result.push(Object)
+            })
+        }
+        return result;
+    }
+    handleChangeSelect = async (selectedOption) => {
+        this.setState({selectedDoctor :selectedOption})
+    };
+    handleOnchangeDatePicker = (date) => {
+        this.setState({
+            currentDate: date[0]
+        })
+    }
     render() {
         return (
-            <React.Fragment>
-                <div>Manage Schedule</div>
-            </React.Fragment>
+            <div className='manage-schedule-container'>
+                <div className='m-s-title'>
+                    <FormattedMessage id="manage-schedule.title" />
+                </div>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-6 form-group'>
+                            <label>Bac si</label>
+                            <Select
+                                value={this.state.selectedDoctor}
+                                onChange={this.handleChangeSelect}
+                                options={this.state.listDoctors}
+                            />
+                        </div>
+                        <div className='col-6 form-group'>
+                            <label>Ngay</label>
+                            <DatePicker
+                            onChange={this.handleOnchangeDatePicker}
+                            className='form-control'
+                            value={this.state.currentDate}
+                            minDate={new Date()}
+                            />
+                        </div>
+                        <div className='col-12 pick-hour-container'>
+                        </div>
+                        <button className='btn btn-primary'>Luu</button>
+                    </div>
+                </div>
+
+            </div>
         );
     }
 }
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+        allDoctors: state.admin.allDoctors
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        fetchAllDoctors: () => dispatch(actions.fetchAllDoctors()),
+
     };
 };
 
